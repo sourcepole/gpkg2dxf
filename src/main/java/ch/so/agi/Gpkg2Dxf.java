c8cbd1e64a8cb853d68de8f87d47d32ca158d7ee
Hussein Kabbout <hussein.kabbout@hotmail.com>
2021-02-16 16:57:08 +0100

    Add prettier logging output

--- src/main/java/ch/so/agi/Gpkg2Dxf.java
+++ src/main/java/ch/so/agi/Gpkg2Dxf.java
@@ -12,6 +12,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.interlis2.av2geobau.impl.DxfUtil;
@@ -34,7 +35,10 @@ import ch.interlis.iox_j.jts.Jts2iox;
 
 import ch.interlis.ioxwkf.gpkg.GeoPackageReader;
 
+import java.util.logging.ConsoleHandler;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Gpkg2Dxf {
 
@@ -57,6 +61,27 @@ public class Gpkg2Dxf {
         } else {
             this.taskName = taskName;
         }
+
+        // This is used to make the log output prettier
+        setLoggerHandler();
+    }
+
+    private void setLoggerHandler() {
+        log.setUseParentHandlers(false);
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setFormatter(new SimpleFormatter() {
+            private static final String format = "[%1$tF %1$tT] [%2$-7s] %3$s %n";
+
+            @Override
+            public synchronized String format(LogRecord lr) {
+                return String.format(format,
+                        new Date(lr.getMillis()),
+                        lr.getLevel().getLocalizedName(),
+                        lr.getMessage()
+                );
+            }
+        });
+        log.addHandler(handler);
     }
 
     public void execute(String gpkgFile, String outputDir) throws Exception {
@@ -338,7 +363,14 @@ public class Gpkg2Dxf {
 
             gpkg2Dxf.execute(gpkgFile, outputDir);
         } catch (Exception exception) {
-            System.out.println(exception);
+            StringBuilder stackTrace = new StringBuilder();
+            stackTrace.append(exception.toString());
+            StackTraceElement[] trace = exception.getStackTrace();
+
+            for (StackTraceElement traceElement : trace)
+                stackTrace.append("\n\tat " + traceElement);
+
+            log.severe(stackTrace.toString());
         }
     }
 }
